ab610e28fe5ec254efbc2cfecd9f35d9d747c618
luke185 <luke4044@protonmail.com>
2023-09-20 16:46:20 +1000

    Add prac 1 files

---
+++ prac_01/menus.py
@@ -0,0 +1,13 @@
+name = input("Enter name: ")
+print("(H)ello\n(G)oodbye\n(Q)uit")
+choice = input(">>> ").upper()
+while choice != 'Q':
+    if choice == 'H':
+        print(f"Hello {name}")
+    elif choice == 'G':
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print("(H)ello\n(G)oodbye\n(Q)uit")
+    choice = input(">>> ").upper()
+print("Finished.")
---
+++ prac_01/shop_calculator.py
@@ -0,0 +1,8 @@
+total_price = 0
+number_of_items = int(input("Number of items: "))
+for i in range(number_of_items):
+    item_price = float(input("Price of item: "))
+    total_price = total_price + item_price
+if total_price > 100:  # apply 10% discount if over $100
+    total_price = total_price - (total_price * 0.1)
+print(f"Total price for {number_of_items} items is ${total_price:.2f}")
