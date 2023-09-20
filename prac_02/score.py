f6f830a1838cdce056a3f5a32d78156110e85fd9
luke185 <luke4044@protonmail.com>
2023-09-20 19:26:10 +1000

    Refactor score.py into programs and add random score

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,14 +1,24 @@
-"""
-CP1404/CP5632 - Practical
-Broken program to determine score status
-"""
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score < 50:
-    print("Bad")
-elif score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+
+def main():
+    score = float(input("Enter score: "))
+    result = determine_result(score)
+    print(f"Your result is {result}")
+    random_score = random.randint(0, 100)
+    random_result = determine_result(random_score)
+    print(f"Random score: {random_score} Random result: {random_result}")
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score < 50:
+        return "Bad"
+    elif score < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
