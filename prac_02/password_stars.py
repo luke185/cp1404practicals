355227a029c834a2cee97979a71a0f8177803bf7
luke185 <luke4044@protonmail.com>
2023-09-20 17:02:46 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,15 @@
-password = input("Password: ")
-while len(password) < 8:
-    print("Password must be at least 8 characters!")
+def main():
+    password = get_password()
+    for i in password:
+        print('*', end='')
+
+
+def get_password():
     password = input("Password: ")
-for i in password:
-    print('*', end='')
+    while len(password) < 8:
+        print("Password must be at least 8 characters!")
+        password = input("Password: ")
+    return password
+
+
+main()
