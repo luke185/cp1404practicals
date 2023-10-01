f52a5b9d5ee699967ee5e395b1805976ab5837c2
luke185 <luke4044@protonmail.com>
2023-10-01 18:56:39 +1000

    add randoms.py to programs

---
+++ prac_03/randoms.py
@@ -0,0 +1,8 @@
+import random
+
+# Smallest: 5 | Largest: 20
+# Smallest: 3 | Largest: 9 | 4? no
+# Smallest:2.5 | Largest: 5.5 | 4? no
+
+print('Here is your random number:')
+print(random.randint(1, 100))
