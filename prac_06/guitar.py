c4122beb388c25f23226be137c413a36e3ced578
luke185 <luke4044@protonmail.com>
2023-10-29 14:09:41 +1000

    complete guitar.py

---
+++ prac_06/guitar.py
@@ -0,0 +1,26 @@
+"""
+CP1404 Prac 6 - Guitar Class
+Estimated time: 15m  (includes testing and reading)
+Actual time: 18m
+"""
+import datetime
+
+
+class Guitar:
+    def __init__(self, name='', year=0, cost=0.0):
+        """Initialise a guitar"""
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Represent a Guitar"""
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        """Return how old the guitar is in years"""
+        return (datetime.date.today()).year - self.year
+
+    def is_vintage(self):
+        """Return True if the guitar is >= 50 years old"""
+        return self.get_age() >= 50
