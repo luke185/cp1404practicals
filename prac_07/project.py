6e2d15cd4e8eeeb65bab18996702c2982985603e
luke185 <luke4044@protonmail.com>
2023-11-05 23:45:12 +1000

    create project class

--- prac_07/project.py
+++ prac_07/project.py
@@ -4,5 +4,16 @@ CP1404 Prac 07 - project.py
 
 
 class Project:
-    def __init__(self):
-        pass
+    def __init__(self, name='', start_date='', priority=0, cost_estimate=0.0, completion_percentage=0):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __repr__(self):
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}," \
+               f"completion: {self.completion_percentage}%"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
