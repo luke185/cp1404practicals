4d5275ab86ac6e77ee4795d7169c0c9a276d8a18
luke185 <luke4044@protonmail.com>
2023-11-15 12:04:14 +1000

    Complete 'Class Variables'

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,6 +1,6 @@
 from taxi import Taxi
 
-my_taxi = Taxi('Pris 1', 100, 1.23)
+my_taxi = Taxi('Pris 1', 100)
 my_taxi.drive(40)
 print(f"{my_taxi}, current fare total is: ${my_taxi.get_fare()}")
 my_taxi.start_fare()
